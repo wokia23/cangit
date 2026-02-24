b6c2b5dc1feb1e19e9cde7be5e1c9f7bf740739d
wokia23 <wokiakumase@gmail.com>
2026-02-24 17:21:33 -0500

    added a server

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "aircanada_ec2" {
-  count         = 2
+  count         = 3
   ami           = var.ec2_ami
   instance_type = var.ec2_instance_type
   subnet_id     = aws_subnet.AC_Public.id
